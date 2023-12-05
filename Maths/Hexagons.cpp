051bc129d0bc7b73c8207213a87de2401e0ebfc8
ano-ny-mous <nesar6174@gmail.com>
2023-12-05 22:53:57 +0530

    Hexagons

---
+++ Maths/Hexagons.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    long long n;
+    cin >> n;
+    long long result = 3 * n * (n + 1) + 1;
+    cout << result << endl;
+    return 0;
+}
